64a9d5165109a9835ae7e45b62483911e5bd1011
Artem <artp956@gmail.com>
2021-04-25 17:41:09 +0300

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import {useQuery} from 'react-query';
+//Components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+//styles
+import { Wrapper } from './StyledComponents/App.styles';
+
+
+function App() {
+  return (
+    <div>
+      Start
+    </div>
+  );
+}
+
+export default App;
